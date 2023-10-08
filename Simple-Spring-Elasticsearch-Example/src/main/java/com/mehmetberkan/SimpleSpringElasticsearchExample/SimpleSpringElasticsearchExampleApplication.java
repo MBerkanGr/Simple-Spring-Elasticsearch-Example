13ece7b4384b60ac304be0f85c3ca4da3b57a781
Mehmet Berkan <mehmetberkangorel@gmail.com>
2023-10-08 20:02:07 +0300

    Initial commit

---
+++ Simple-Spring-Elasticsearch-Example/src/main/java/com/mehmetberkan/SimpleSpringElasticsearchExample/SimpleSpringElasticsearchExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mehmetberkan.SimpleSpringElasticsearchExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringElasticsearchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringElasticsearchExampleApplication.class, args);
+	}
+
+}
---
+++ Simple-Spring-Elasticsearch-Example/src/test/java/com/mehmetberkan/SimpleSpringElasticsearchExample/SimpleSpringElasticsearchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmetberkan.SimpleSpringElasticsearchExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringElasticsearchExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
