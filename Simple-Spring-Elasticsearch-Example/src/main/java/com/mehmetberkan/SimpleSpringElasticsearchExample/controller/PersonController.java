b709035e0280810618eeea96f3202af11d5c769b
Mehmet Berkan <mehmetberkangorel@gmail.com>
2023-10-11 21:52:39 +0300

    Add Phrase, Fuzziness and FullText search methods

--- Simple-Spring-Elasticsearch-Example/src/main/java/com/mehmetberkan/SimpleSpringElasticsearchExample/controller/PersonController.java
+++ Simple-Spring-Elasticsearch-Example/src/main/java/com/mehmetberkan/SimpleSpringElasticsearchExample/controller/PersonController.java
@@ -2,6 +2,7 @@ package com.mehmetberkan.SimpleSpringElasticsearchExample.controller;
 
 import com.mehmetberkan.SimpleSpringElasticsearchExample.model.Person;
 import com.mehmetberkan.SimpleSpringElasticsearchExample.service.PersonService;
+import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,5 +31,30 @@ public class PersonController {
     @GetMapping("/transactions/{transactionValue}")
     public ResponseEntity<List<Person>> getByTransactionValue(@PathVariable String transactionValue) {
         return ResponseEntity.ok(personService.getByTransactionValue(transactionValue));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<SearchHit<Person>>> search(@RequestParam String search) {
+        return ResponseEntity.ok(personService.getPersonsByDescription(search));
+    }
+
+    @GetMapping("/search-score")
+    public ResponseEntity<List<SearchHit<Person>>> searchByScoreDescription(@RequestParam String search) {
+        return ResponseEntity.ok(personService.getPersonsByScore(search));
+    }
+
+    @GetMapping("/search-fulltext")
+    public ResponseEntity<List<SearchHit<Person>>> searchByFullText(@RequestParam String search) {
+        return ResponseEntity.ok(personService.getPersonsByFullText(search));
+    }
+
+    @GetMapping("/search-fuzziness")
+    public ResponseEntity<List<SearchHit<Person>>> searchByFuzziness(@RequestParam String search) {
+        return ResponseEntity.ok(personService.getPersonByFuzziness(search));
+    }
+
+    @GetMapping("/search-phrase")
+    public ResponseEntity<List<SearchHit<Person>>> searchByPhrase(@RequestParam String search) {
+        return ResponseEntity.ok(personService.getPersonsByPhrase(search));
     }
 }
--- Simple-Spring-Elasticsearch-Example/src/main/java/com/mehmetberkan/SimpleSpringElasticsearchExample/service/PersonService.java
+++ Simple-Spring-Elasticsearch-Example/src/main/java/com/mehmetberkan/SimpleSpringElasticsearchExample/service/PersonService.java
@@ -2,6 +2,8 @@ package com.mehmetberkan.SimpleSpringElasticsearchExample.service;
 
 import com.mehmetberkan.SimpleSpringElasticsearchExample.model.Person;
 import com.mehmetberkan.SimpleSpringElasticsearchExample.repository.PersonRepository;
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.Operator;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
@@ -14,15 +16,20 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.elasticsearch.index.query.QueryBuilders.matchPhraseQuery;
 import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 
 @Service
 public class PersonService {
 
     private final PersonRepository personRepository;
+    private final ElasticsearchOperations elasticsearchOperations;
+    private final String matchQueryConstant = "description";
+    private final String indexNameConstant = "persons";
 
-    public PersonService(PersonRepository personRepository) {
+    public PersonService(PersonRepository personRepository, ElasticsearchOperations elasticsearchOperations) {
         this.personRepository = personRepository;
+        this.elasticsearchOperations = elasticsearchOperations;
     }
 
     public Person addPerson(Person person) {
@@ -45,5 +52,56 @@ public class PersonService {
                 .findByTransactionsValueUsingCustomQuery(transactionValue, PageRequest.of(0, 10));
 
         return personPage.getContent();
+    }
+
+    public List<SearchHit<Person>> getPersonsByDescription(String search) {
+
+        NativeSearchQuery query = new NativeSearchQueryBuilder()
+                .withQuery(matchQuery(indexNameConstant, search))
+                .build();
+
+        return elasticsearchOperations.search(query, Person.class,
+                IndexCoordinates.of()).getSearchHits();
+    }
+
+
+    public List<SearchHit<Person>> getPersonsByScore(String searchTerm) {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(matchQuery(matchQueryConstant, searchTerm)
+                        .minimumShouldMatch("100%"))
+                .build();
+        return elasticsearchOperations.search(searchQuery, Person.class,
+                IndexCoordinates.of(indexNameConstant)).getSearchHits();
+    }
+
+    public List<SearchHit<Person>> getPersonsByFullText(String searchTerm) {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(matchQuery(matchQueryConstant, searchTerm)
+                        .operator(Operator.AND))
+                .build();
+
+        return elasticsearchOperations.search(searchQuery, Person.class,
+                IndexCoordinates.of(indexNameConstant)).getSearchHits();
+    }
+
+    public List<SearchHit<Person>> getPersonByFuzziness(String searchTerm) {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(matchQuery(matchQueryConstant, searchTerm)
+                        .operator(Operator.AND)
+                        .fuzziness(Fuzziness.ONE)
+                        .prefixLength(2))
+                .build();
+
+        return elasticsearchOperations.search(searchQuery, Person.class,
+                IndexCoordinates.of(indexNameConstant)).getSearchHits();
+    }
+
+    public List<SearchHit<Person>> getPersonsByPhrase(String searchTerm) {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(matchPhraseQuery(matchQueryConstant, searchTerm).slop(1))
+                .build();
+
+        return elasticsearchOperations.search(searchQuery, Person.class,
+                IndexCoordinates.of(indexNameConstant)).getSearchHits();
     }
 }
